034651277c20eee9429df75c71412666eb8dc8f7
papaocean <120007868+papaocean@users.noreply.github.com>
2023-01-12 15:16:56 +0100

    MUSIK

---
+++ Assets/c# scripts/soundvolumeslider.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+ public class soundvolumeslider : MonoBehaviour
+{
+    [SerializeField] Slider SliderVolume;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("musicVolume")) 
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            Loading();
+        }
+        else
+        {
+            Loading();
+        }
+    }
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = SliderVolume.value;
+        SaveSetting();
+    }
+
+    private void Loading()
+    {
+
+        SliderVolume.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    private void SaveSetting()
+    {
+
+        PlayerPrefs.SetFloat("musicVolume", SliderVolume.value);
+    }
+}
