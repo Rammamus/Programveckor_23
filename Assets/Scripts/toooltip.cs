f4e48d343556cc7a3bd16fba71f06cdf7ed1a6bf
Cc4sper <casper.kardelltranetoft@elev.ga.lbs.se>
2023-01-17 11:09:47 +0100

    textfont

---
+++ Assets/Scripts/toooltip.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toooltip : MonoBehaviour
+{
+    public string messasge;
+
+
+
+    Vector3 mousePos = Input.mousePosition;
+    
+   
+   
+
+
+    /* tooltip._instance.HideToolTip(); 
+     * 
+     * tooltip._instance.SetAndShowToolTip(messasge);
+     */
+}
