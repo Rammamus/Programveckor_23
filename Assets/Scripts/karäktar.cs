6a65c7d3de8bc478c6bf2847f99a8a59c3bc0818
williamfedko <william.svenssonfedko@elev.ga.lbs.se>
2023-01-18 11:26:54 +0100

    boss2

--- Assets/Scripts/karäktar.cs
+++ Assets/Scripts/karäktar.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class kar�ktar : MonoBehaviour
+public class karäktar : MonoBehaviour
 
 {
     //Variables for attacking - Adrian
