35902cae00da5a0b79efc7bb939c6b092736696d
Rammamus <adrian.folin@elev.ga.lbs.se>
2023-01-18 13:53:23 +0100

    pause menu

--- Assets/Scripts/DeathScreen.cs
+++ Assets/Scripts/DeathScreen.cs
@@ -14,6 +14,15 @@ public class DeathScreen : MonoBehaviour
     public void Activate_pausemenu()
     {
         pausePanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void DeActivate_pausemenu()
+    {
+        pausePanel.SetActive(false);
+        gamePaused = false;
+        Time.timeScale = 1;
+        print("deactive");
     }
     public void Activate_deathscreen()
     {
@@ -24,17 +33,12 @@ public class DeathScreen : MonoBehaviour
         //condition for game pause - Adrian
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            gamePaused = true;
+            Activate_pausemenu();
         }
         //condition for death screen - Adrian
         if (player.GetComponent<karäktar>().playHP <= 0)
         {
             deathScreen = true;
-        }
-
-        if (gamePaused)
-        {
-            Activate_pausemenu();
         }
         if (deathScreen)
         {
