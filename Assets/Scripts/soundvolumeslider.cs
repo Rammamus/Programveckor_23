48398488d581d5229f93f91313001065dda3c00d
williamfedko <william.svenssonfedko@elev.ga.lbs.se>
2023-01-20 10:29:07 +0100

    namn

--- Assets/Scripts/soundvolumeslider.cs
+++ Assets/Scripts/soundvolumeslider.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// fungerande ljudslider - Ocean 
  public class soundvolumeslider : MonoBehaviour
 {
     [SerializeField] Slider SliderVolume;
