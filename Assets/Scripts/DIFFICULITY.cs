587eb91247795c8892ad8f7adbb03efe97099a16
Rammamus <adrian.folin@elev.ga.lbs.se>
2023-02-01 10:16:06 +0100

    difficulty little buit

--- Assets/Scripts/DIFFICULITY.cs
+++ Assets/Scripts/DIFFICULITY.cs
@@ -13,19 +13,35 @@ public class DIFFICULITY : MonoBehaviour
     }
     public void Easydiff()
     {
+        StaticVariableHolder.staticEasy = true;
+        StaticVariableHolder.staticMedium = false;
+        StaticVariableHolder.staticHard = false;
+        StaticVariableHolder.staticImpossible = false;
         em.gameObject.GetComponent<Enemy>().isEasy = true;
     }
     public void Mediumdiff()
     {
+        StaticVariableHolder.staticEasy = false;
+        StaticVariableHolder.staticMedium = true;
+        StaticVariableHolder.staticHard = false;
+        StaticVariableHolder.staticImpossible = false;
         em.gameObject.GetComponent<Enemy>().isMedium = true;
     }
     public void HardDiff()
     {
+        StaticVariableHolder.staticEasy = false;
+        StaticVariableHolder.staticMedium = false;
+        StaticVariableHolder.staticHard = true;
+        StaticVariableHolder.staticImpossible = false;
         em.gameObject.GetComponent<Enemy>().isHard = true;
         
     }
     public void ImpossibleDiff()
     {
+        StaticVariableHolder.staticEasy = false;
+        StaticVariableHolder.staticMedium = false;
+        StaticVariableHolder.staticHard = false;
+        StaticVariableHolder.staticImpossible = true;
         em.gameObject.GetComponent<Enemy>().isImpossible = true;
         
     }
--- Assets/Scripts/StaticVariableHolder.cs
+++ Assets/Scripts/StaticVariableHolder.cs
@@ -2,7 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//This script handles variables that will not reset after scene changes - Adrian
 public class StaticVariableHolder : MonoBehaviour
 {
     public static int permaCoins;
-}
+    public static bool staticEasy = false;
+    public static bool staticMedium = false;
+    public static bool staticHard = false;
+    public static bool staticImpossible = false;
+}       
